c35b92611af78cf8d211ce5866d47b62d59d3432
Enis <78642947+enis26@users.noreply.github.com>
2024-10-25 17:29:14 +0300

    2024 enflasyon update
    alpin ami patladi gorunce

--- tablolar.h
+++ tablolar.h
@@ -4,22 +4,22 @@
 char icecektablosu[512] = "                     \n\
         İçecekler        |          Alkol       \n\
 -------------------------|-----------------------\n\
-1 - Alp Cola : 5TL       | 4 - Bira : 20TL       \n\
-2 - Enginar Suyu : 10TL  | 5 - Şarap : 30TL      \n\
-3 - Havuç Suyu : 15TL    | 6 - Şampanya : 50TL   \n\
+1 - Alp Cola : 30TL       | 4 - Bira : 100TL       \n\
+2 - Enginar Suyu : 70TL  | 5 - Şarap : 500TL      \n\
+3 - Havuç Suyu : 40TL    | 6 - Şampanya : 350TL   \n\
 ";
 
 char yiyecektablosu[512] = "                          \n\
     Ana Yemekler        |   Diğer yiyecekler           \n\
 ------------------------|--------------------           \n\
-1 - Kurufasulye : 10TL  | 4 - Hıyar(adanali olan) : 20TL \n\
-2 - Izgara Köfte : 25TL | 5 - Domates : 5TL               \n\
-3 - Adana Kebap : 20TL  | 6 - Enginar : 30TL               \n\
+1 - Kurufasulye : 80TL  | 4 - Hıyar(adanali olan) : 45TL \n\
+2 - Izgara Köfte : 250TL | 5 - Domates : 65TL               \n\
+3 - Adana Kebap : 150TL  | 6 - Enginar (Soyulmus) : 600TL               \n\
 ";
 char oyuntablosu[512] = "\n\
                  Oyunlar\n\
 -----------------------------------------\n\
-1 - Bowling : 10TL  |   2 - Bilardo : 20TL\n\
+1 - Bowling : 200  |   2 - Bilardo : 150TL\n\
 ";
 
 int yiyecek_fiyat(int no) {
@@ -37,7 +37,7 @@ int yiyecek_fiyat(int no) {
         case 6:
             return(30);
         default:
-            printf("Böyle bir yiyecek yok!\n");
+            printf("siktir git amerikadamiyiz oc\n");
             exit(1);
     }
 }
@@ -57,7 +57,7 @@ int icecek_fiyat(int no) {
         case 6:
             return(50);
         default:
-            printf("Böyle bir içecek yok!");
+            printf("haram lan o - Mashiro");
             return 0;
     }
 }
@@ -69,7 +69,7 @@ int oyun_fiyat(int no) {
         case 2:
             return(20);
         default:
-            printf("Böyle bir oyun yok!");
+            printf("hepsi amerikanin oyunu - mert usta");
             return 0;
     }
 }
