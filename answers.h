d872d3bdc207b02f943e2858542f3bfe60f4810d
Mert Özkan <48324592+mrtozkn@users.noreply.github.com>
2024-10-25 17:42:39 +0300

    31 update yapıldı
    am_göt_meme_yarrak_tassak_31
    YAOPILANLARDAN GILGIL PLAY SORUMNLUDUR

--- answers.h
+++ answers.h
@@ -1,29 +1,29 @@
 #include <stdio.h>
 
-char ahmet_cevap[256] = "\n\
+char amet_cevap[256] = "\n\
+1. Ezbırçime\n\
+2. belə vəziyyət içine soxum\n\
+";
+
+char demirtas_cevap[256] = "\n\
 1. Merhaba\n\
 2. Naber\n\
 ";
 
-char mehmet_cevap[256] = "\n\
-1. Merhaba\n\
-2. Naber\n\
-";
-
-char john_cevap[256] = "\n\
-1. Hi!\n\
-2. How are you?\n\
+char ocalan_cevap[256] = "\n\
+1. am_cuk_yarrak_tassak_31\n\
+2. zencigot\n\
 ";
 
 void answer(char* name) {
-    	if (strcmp(name, "Ahmet") == 0 || strcmp(name, "ahmet") == 0) {
-    	char *answer1 = "%s : I dont want to talk!", name;
-    	char *answer2 = "%s : Hey bro!", name;
-    	char *answer3 = "%s : Beautiful place, right?", name;
-    	char *answer4 = "%s : A fucking place", name;
+    	if (strcmp(name, "Amet") == 0 || strcmp(name, "amet") == 0) {
+    	char *answer1 = "%s : Sikismek istiyorum", name;
+    	char *answer2 = "%s : Zencigot severim", name;
+    	char *answer3 = "%s : Essek_siker_31", name;
+    	char *answer4 = "%s : kurt_ami_sever_31", name;
     	char *arr[50] = {(answer1[rand()%5]), (answer2[rand() % 5]), (answer3[rand() % 5]), (answer4[rand() % 5])};
 	printf("%s", arr);
-	konus();
+	sikis();
     }
 	else {
 	     /*Other people will be added*/
